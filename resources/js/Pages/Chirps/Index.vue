d3552448562e183d1bb83d4eaf5f0bd1f73476d4
Yeison Sanabria <yeisonsanabria@iMac-de-Yeison.local>
2024-02-05 11:07:38 -0500

    edit chirp-layout

---
+++ resources/js/Pages/Chirps/Index.vue
@@ -0,0 +1,28 @@
+<script setup>
+import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
+import { Head, Link } from '@inertiajs/vue3';
+defineProps(["title", "subtitle"])
+</script>
+
+<template>
+    <Head title="Chirps" />
+
+    <AuthenticatedLayout>
+        <template #header>
+            <h2 class="font-semibold text-xl text-gray-800 dark:text-gray-200 leading-tight">Dashboard
+                {{ title }}
+            </h2>
+            
+        </template>
+
+        <div class="py-12">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+                <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
+                    <div class="p-6 text-gray-900 dark:text-gray-100">
+                        <Link :href="route('dashboard')"> Dashboard </Link> 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </AuthenticatedLayout>
+</template>
