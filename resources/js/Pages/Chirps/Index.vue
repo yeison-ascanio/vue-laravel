d3fd31c4816c0ff5cd7b608bd562f720f0ef6005
Yeison David Sanabria Ascanio <yeison@ovy.co>
2024-02-05 17:56:38 -0500

    implement useForm for chirps send

--- resources/js/Pages/Chirps/Index.vue
+++ resources/js/Pages/Chirps/Index.vue
@@ -1,33 +1,20 @@
 <script setup>
 import AuthenticatedLayout from '@/Layouts/AuthenticatedLayout.vue';
 import PrimaryButton from '@/Components/PrimaryButton.vue';
-import { Head, Link } from '@inertiajs/vue3';
+import { Head, Link, useForm } from '@inertiajs/vue3';
 import { ref } from 'vue';
 import axios from 'axios';
 import InputError from '@/Components/InputError.vue';
 
-defineProps(["title", "subtitle"])
-
-const message = ref('')
-const errors = ref('')
-const processing = ref(false)
+const form = useForm({
+    message: ''
+})
 
 function submit() {
-    processing.value = true
-    axios.post(route('chirps.store'), { message: message.value })
-        .then((res) => {
-            console.log(res.data);
-            message.value = ""
-            errors.value = {}
-        })
-        .catch((err) => {
-            if (err.response.status === 422) {
-                errors.value = err.response.data.errors
-                return
-            }
-            console.log(err.response.data.message);
-        }).finally(() => (processing.value = false))
-} 
+    form.post(route('chirps.store'), {
+        onSuccess: (res) => form.reset()
+    })
+}
 </script>
 
 <template>
@@ -46,12 +33,12 @@ function submit() {
                 <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
                     <div class="p-6 text-gray-900 dark:text-gray-100">
                         <form @submit.prevent="submit">
-                            <textarea placeholder="what's on your mind?" v-model="message"
+                            <textarea placeholder="what's on your mind?" v-model="form.message"
                                 class="bg-gray-800 text-white p-2 block w-full rounded-md border-gray-300 p-2 border-2 border-gray-500 focus:border-emerald-500 outline-color:emerald-500 placeholder-gray-500">
                             </textarea>
-                            <InputError :message="errors.message && errors.message[0]" />
-                            <PrimaryButton :disable="processing" class="mt-2">
-                                {{ processing ? "Enviando..." : "Chirps" }}
+                            <InputError :message = "form.errors.message" />
+                            <PrimaryButton :disable= "form.processing" class="mt-2">
+                                {{ form.processing ? "Enviando..." : "Chirps" }}
                             </PrimaryButton>
                         </form>
                     </div>
